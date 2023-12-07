9dd14e6f30dc262eb86ce2e4d21253adeb1ff4f7
dfppr <107608037+dfppr@users.noreply.github.com>
2023-12-07 19:45:19 +0700

    Добавьте файлы проекта.

---
+++ Controllers/FormController.cs
@@ -0,0 +1,30 @@
+﻿using App_GPO.Models;
+using System.Diagnostics;
+using DocuSign.eSign.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace App_GPO.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            //string[] source = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", };
+            //SelectList selectlist = new SelectList(source, source[2]);
+            //ViewBag.SelectItems = SelectList;
+            return View();
+        }
+
+        [HttpPost] 
+        public IActionResult Check(Form form)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+
+    }
+}
---
+++ Models/Form.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App_GPO.Models
+{
+    public class Form
+    {
+        [Display(Name = "ФИО")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string? FullName { get; set; }
+
+        [Display(Name = "Возраст")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public int Age { get; set; }
+
+        [Display(Name = "Образование")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string? Education { get; set; }
+
+        [Display(Name = "Дополнительное образование (курсы повышения квалицикации, профессиональная переподготовка)")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string? AddEducation { get; set; }
+
+        [Display(Name = "Компетенции")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string? Competence { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string? Email { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        [Required(ErrorMessage = "Заполните поле")]
+        public string? PhoneNumber { get; set; }
+
+        [Display(Name = "Дополнительная информация")]
+        public string? AddInfo { get; set; }
+    }
+
+}
