ed2ddb5677e06b8afb8801ec535411d1b9c0ed8c
dfppr <107608037+dfppr@users.noreply.github.com>
2023-12-07 22:18:43 +0700

    Поле "Дата рождения" вместо поля "Возраст", radionbutton для полей "Дополнительное образование" и  "Опыт работы"

--- Models/Form.cs
+++ Models/Form.cs
@@ -8,21 +8,21 @@ namespace App_GPO.Models
         [Required(ErrorMessage = "Заполните поле")]
         public string? FullName { get; set; }
 
-        [Display(Name = "Возраст")]
+        [Display(Name = "Дата рождения")]
         [Required(ErrorMessage = "Заполните поле")]
-        public int Age { get; set; }
+        public int Birthday { get; set; }
 
         [Display(Name = "Образование")]
         [Required(ErrorMessage = "Заполните поле")]
         public string? Education { get; set; }
 
-        [Display(Name = "Дополнительное образование (курсы повышения квалицикации, профессиональная переподготовка)")]
+        [Display(Name = "Дополнительное образование")]
         [Required(ErrorMessage = "Заполните поле")]
         public string? AddEducation { get; set; }
 
-        [Display(Name = "Компетенции")]
+        [Display(Name = "Опыт работы")]
         [Required(ErrorMessage = "Заполните поле")]
-        public string? Competence { get; set; }
+        public string? Experience { get; set; }
 
         [Display(Name = "Email")]
         [Required(ErrorMessage = "Заполните поле")]
